67da0dfd98bd8a7fc452d7c0b85831656aaac083
Cavan Beardmore <106985709+CavanBeardmore@users.noreply.github.com>
2023-08-21 10:08:09 +0100

    Updated sign in test

--- cypress/e2e/Generic/sign-in.cy.js
+++ cypress/e2e/Generic/sign-in.cy.js
@@ -2,7 +2,7 @@ describe("sign in portal;", () => {
 
   it("tests sign in portal;", () => {
     cy.viewport(1920, 1080);
-    cy.visit("https://givenergy.cloud");
+    cy.visit("https://givenergy.cloud/login");
     cy.get("#input-22").click();
     cy.get("#input-22").type("chemical_lane");
     cy.get("#input-25").type("passwordplease!");
