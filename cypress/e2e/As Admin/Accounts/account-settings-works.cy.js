f77abbc0b46eefa0b144693495a8731b5165c13f
Cavan Beardmore <106985709+CavanBeardmore@users.noreply.github.com>
2023-08-22 16:30:32 +0100

    Update account-settings-works.cy.js

--- cypress/e2e/As Admin/Accounts/account-settings-works.cy.js
+++ cypress/e2e/As Admin/Accounts/account-settings-works.cy.js
@@ -4,7 +4,9 @@ import { adminLogin } from "../../../funcs";
 describe("Account settings work", () => {
     it("tests Account settings work", () => {
       cy.viewport(1920, 1080);
+      //logs in
       adminLogin()
+      //goes to account details and changes details
       cy.get("#account\\.settings\\.index").click();
       cy.get('label').contains('Username').next('input').clear().type('GivQA1');
       cy.get('label').contains('First Name').next('input').clear().type('Ross1');
@@ -14,6 +16,8 @@ describe("Account settings work", () => {
       cy.get('label').contains('Telephone Number').next('input').clear().type('012345678977');
       cy.get('label').contains('Email Address').next('input').clear().type('ross.coates@givenergy.com');
       cy.get('button').contains('Submit').click();
+      cy.get('p').contains('Account details updated successfully');
+      //changes details back
       cy.get('label').contains('Username').next('input').clear().type('GivQA');
       cy.get('label').contains('First Name').next('input').clear().type('Ross');
       cy.get('label').contains('Surname').next('input').clear().type('Coates');
@@ -22,7 +26,9 @@ describe("Account settings work", () => {
       cy.get('label').contains('Telephone Number').next('input').clear().type('01234567897');
       cy.get('label').contains('Email Address').next('input').clear().type('ross.coates@givenergy.co.uk');
       cy.get('button').contains('Submit').click();
-      //
+      cy.get('p').contains('Account details updated successfully');
+      //goes to manage security and changes password
+      cy.get('button').contains('Manage Account Security').click();
       cy.get("#input-433").click();
       cy.get("#input-433").type(Cypress.env('adminPassword'));
       cy.get("div:nth-of-type(2) div.v-input__slot > div").click();
