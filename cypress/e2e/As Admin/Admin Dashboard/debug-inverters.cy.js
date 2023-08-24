30d761a6a0a79e4969d76e8b73d1a6571a3e9511
Cavan Beardmore <106985709+CavanBeardmore@users.noreply.github.com>
2023-08-24 15:09:03 +0100

    Update debug-inverters.cy.js

--- cypress/e2e/As Admin/Admin Dashboard/debug-inverters.cy.js
+++ cypress/e2e/As Admin/Admin Dashboard/debug-inverters.cy.js
@@ -1,16 +1,48 @@
-import { adminLogin } from "../../../funcs";
+import { adminLogin } from "../../../logins";
 
 describe("debug inverters", () => {
     it("tests debug inverters", () => {
       cy.viewport(1920, 1080);
       adminLogin()
-      cy.get("#admin\\.dashboard\\.index").click();
-      cy.get("div:nth-of-type(2) div:nth-of-type(6) div.v-card__subtitle").click();
-      cy.get(".v-btn v-btn--is-elevated v-btn--has-bg theme--light v-size--default secondary").click();
-      cy.get("#input-6395").click();
-      cy.get("#input-6395").type("SA2249G690");
-      cy.get("#list-item-6408-0 > div > div").click();
-      cy.get("button.primary > span").click();
-      cy.get("tbody > tr:nth-of-type(1) i").click();
+
+      //navigates to the debug inverter page
+      cy.get(".v-navigation-drawer__content").click().get('div').contains('Admin Dashboard').click();
+      cy.get('div').contains('Debug Inverters').click();
+
+      //checks first entry in table and ensure format is correct and button works
+      cy.get('div').contains('Debug Inverters');
+      cy.get('tr').find('td').first().contains(/^[A-Z]{2,}\w{5,}[0-9]{3,}/);
+      cy.get('tr').find('td').first().next().contains(/^[0-9]{1,2}\s[a-zA-Z]{7,}/);
+      cy.get('tr').find('td').first().next().next().contains(/^[a-zA-Z]{1,}\s[a-zA-Z]{1,}/);
+      cy.get('tr').find('td').first().next().next().next().next()
+        .contains(/^\d{4,}[-]\d{2,}[-]\d{2,}\s\d{2,}[:]\d{2,}[:]\d{2,}\s[(]\d{1,}\s[a-z]{5,}\s[a-z]{3,}[)]/);
+      cy.get('tr').find('td').first().next().next().next().next().next()
+        .contains(/^\w{5,}|^\d{4,}[-]\d{2,}[-]\d{2,}\s\d{2,}[:]\d{2,}[:]\d{2,}\s[(]\d{1,}\s[a-z]{7,}\s[a-z]{4,}\s[a-z]{3,}[)]|^\w{5,}|^\d{4,}[-]\d{2,}[-]\d{2,}\s\d{2,}[:]\d{2,}[:]\d{2,}\s[(]\d{1,}\s[a-z]{7,}\s[a-z]{3,}[)]/);
+      cy.get('tr').find('td').first().next().next().next().next().next().next()
+        .contains(/^\w{5,}|^\d{4,}[-]\d{2,}[-]\d{2,}\s\d{2,}[:]\d{2,}[:]\d{2,}$/);
+      cy.get('tr').find('td').last().click();
+
+      //checks add another
+      cy.get('div').contains('Add Another').click();
+      cy.get('div').contains('Add Another Debug Inverter');
+      cy.get('label').contains('Search for an Inverter...').next().type('ED2253G566');
+      cy.wait(1500);
+      cy.get("span").contains('ED2253G566').click();
+      cy.get("span").contains('Cancel').click();
+      cy.get('div').contains('Add Another').click();
+      cy.get("span").contains('Confirm').click();
+
+      //checks search and checks format again
+      cy.get('label').contains('Search').next().type('ED2253G566');
+      cy.get('tr').find('td').first().contains(/^[A-Z]{2,}\w{5,}[0-9]{3,}/);
+      cy.get('tr').find('td').first().next().contains(/^[0-9]{1,2}\s[a-zA-Z]{7,}/);
+      cy.get('tr').find('td').first().next().next().contains(/^[a-zA-Z]{1,}\s[a-zA-Z]{1,}/);
+      cy.get('tr').find('td').first().next().next().next().next()
+        .contains(/^\d{4,}[-]\d{2,}[-]\d{2,}\s\d{2,}[:]\d{2,}[:]\d{2,}\s[(]\d{1,}\s[a-z]{5,}\s[a-z]{3,}[)]/);
+      cy.get('tr').find('td').first().next().next().next().next().next()
+        .contains(/^\w{5,}|^\d{4,}[-]\d{2,}[-]\d{2,}\s\d{2,}[:]\d{2,}[:]\d{2,}\s[(]\d{1,}\s[a-z]{7,}\s[a-z]{4,}\s[a-z]{3,}[)]|^\w{5,}|^\d{4,}[-]\d{2,}[-]\d{2,}\s\d{2,}[:]\d{2,}[:]\d{2,}\s[(]\d{1,}\s[a-z]{7,}\s[a-z]{3,}[)]/);
+      cy.get('tr').find('td').first().next().next().next().next().next().next()
+        .contains(/^\w{5,}|^\d{4,}[-]\d{2,}[-]\d{2,}\s\d{2,}[:]\d{2,}[:]\d{2,}$/);
+      cy.get('tr').find('td').last().click();
     });
   });
