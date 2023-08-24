dbfc976d3f32ba15b0c50011163e76bdcbb29a56
Cavan Beardmore <106985709+CavanBeardmore@users.noreply.github.com>
2023-08-24 15:35:05 +0100

    Update notifications.cy.js

--- cypress/e2e/As Admin/Admin Dashboard/notifications.cy.js
+++ cypress/e2e/As Admin/Admin Dashboard/notifications.cy.js
@@ -1,30 +1,33 @@
-import { adminLogin } from "../../../funcs";
+import { adminLogin } from "../../../logins";
 
-//DOESNT WORK CAN'T CLICK CREATE NOTIFICATION
-describe("Recording 22/08/2023 at 11:23:47", () => {
-    it("tests Recording 22/08/2023 at 11:23:47", () => {
+describe("notifications", () => {
+    it("tests notifications", () => {
       cy.viewport(1920, 1080);
       cy.visit("https://givenergy.cloud/dashboard");
       adminLogin()
-      cy.get("#admin\\.dashboard\\.index").click();
-      cy.get("div:nth-of-type(2) div:nth-of-type(9) div.v-card__title").click();
-      cy.get("div.col span").click();
-      //HERE^
-      cy.get("#input-559").click();
-      cy.get("#list-item-590-1 > div > div").click();
-      cy.get("#input-596").click();
-      cy.get("#input-596").type("demo user");
-      cy.get("#list-item-603-0 > div > div").click();
-      cy.get("#input-564").click();
-      cy.get("#input-564").type("Test");
-      cy.get("#input-567").click();
-      cy.get("#input-567").type("Hello!");
-      cy.get("form > div > div:nth-of-type(4) span").click();
-      cy.get("form > div > div:nth-of-type(4) button").click();
-      cy.get("#input-571").click();
-      cy.get("#input-571").type("Testing 123");
-      cy.get("div.v-application--wrap div.v-card__actions span").click();
-      cy.get("div.v-dialog__content button.secondary > span").click();
+
+      //navigates to notifications
+      cy.get(".v-navigation-drawer__content").click().get('div').contains('Admin Dashboard').click();
+      cy.get('div').contains('Send an letterheaded email to one or more customers via the portal').click();
+      cy.get('div').contains('Pending Notifications');
+
+      //send notification
+      cy.get('span').contains('Create Notification').click();
+      cy.get('div').contains('Generate Notification');
+      cy.get('label').contains('Send To').next().click();
+      cy.get('div').contains('Specific Account').click();
+      cy.get('label').contains('Search for Account').next().type('Cavan Beardmore');
+      cy.get('.v-application--wrap').next().next().next().find('div').find('div').first().click();
+      cy.get('label').contains('Subject').next().type('Test Subject');
+      cy.get('label').contains('Greeting (Optional)').next().type('Test Greeting');
+      cy.get('span').contains('Add Action').click();
+      cy.get('label').contains('Action Text').next().type('Test');
+      cy.get('label').contains('Action URL').next().type('https://givenergy.cloud/dashboard');
+      cy.get('label').contains('Paragraph 1').next().type('Test Paragraph');
+      cy.get('span').contains('Submit').click();
+      cy.get('span').contains('Cancel').click();
+      cy.get('span').contains('Submit').click();
+      cy.get('span').contains('Send Notification').click();
     });
   });
   
