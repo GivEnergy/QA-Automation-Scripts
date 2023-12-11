3dcaa66a6a04ca96841de696d8aa5d73eff0178b
CavanBeardmore <cavan.beardmore@hotmail.co.uk>
2023-12-11 15:35:04 +0000

    Reward Points test:
    added a test for the admin rewards section and history pages

--- cypress/regex.js
+++ cypress/regex.js
@@ -4,3 +4,4 @@ export const serialNumber = /^[A-Z]{2,2}\w{5,5}\d{3,3}/
 export const batteryFW = /^\d{1,4}$/
 export const inverterFW = /^[D][0][.]{1,1}\d{3,3}[-]{1,1}[A][0][.]{1,1}\d{3,3}$/
 export const positiveOrNegativeNumber = /^\d{1,}$|^[-]\d{1,}$/
+export const positiveNumber = /^\d{1,}$/
