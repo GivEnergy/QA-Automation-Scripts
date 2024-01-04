9489cbb9feb5868955df5ad141087f2848089610
CavanBeardmore <cavan.beardmore@hotmail.co.uk>
2024-01-04 11:32:15 +0000

    Added power graph test

--- cypress/dashboardCards.js
+++ cypress/dashboardCards.js
@@ -3,3 +3,5 @@ export const energyGraphDescription = 'Displays historical energy flows';
 export const rewardsTableHeaders = ["Type", "Amount", "Added By", "Time", "Via"];
 
 export const solarForecastDescription = "Use Solcast's API to predict your solar generation";
+
+export const powerGraphDescription = "Show historical data from your system";
---
+++ cypress/e2e/Portal/Dashboard Cards/power-graph.cy.js
@@ -0,0 +1,65 @@
+import { adminLogin } from "../../../logins";
+import {dashboardSelect} from "../../../funcs";
+import {powerGraphDescription} from "../../../dashboardCards";
+import {powerGraphItems} from "../../../listOfValues";
+
+//this should prevent any tests from hanging
+const time = 180000;
+beforeEach(() => {
+    setTimeout(() => {
+        throw new Error(`Test failed: exceeded run time limit of ${time}ms`);
+    }, time);
+});
+describe("power graph card", () => {
+    it("tests power graph card", () => {
+
+        adminLogin();
+        //creates alias for dashboard API request
+        cy.intercept('**/staging.givenergy.cloud/dashboard').as('dashboardAPI');
+        dashboardSelect('Dashboard Cards');
+
+        //creates alias for power graph API request
+        cy.intercept('**/BrymboPVTest/power-graph').as('powerGraphAPI');
+
+        cy.get('[data-qa="card.title"]').each(($div2, index) => {
+
+            const text = $div2.text();
+            console.log(text);
+            if (text === 'Power Graph') {
+                cy.get('[data-qa="card.description"]').as('description');
+                cy.get('@description').eq(index).contains(powerGraphDescription).as('target');
+                cy.get('@target').click();
+                cy.get('@target').scrollIntoView();
+                cy.get('[data-qa="button.search"]').eq(index).click();
+                cy.get('[data-qa="search"]').eq(1).type('brymbo');
+                cy.get('div[class="v-list-item__title"]').contains('BrymboPVTest').click();
+                cy.get('[data-qa="button.view"]').click();
+            }
+        });
+
+        //checks url contains /power-graph/
+        //checks charts exist and are visible
+        //waits for power graph API request to be successful
+        cy.wait('@powerGraphAPI', {timeout: 30000});
+
+        //check elements are visible
+        cy.get('[data-qa="row.powerGraph"]').should('be.visible');
+        cy.get('[data-qa="slot.datePicker"]').should('be.visible');
+        cy.get('[data-qa="autocomplete.items"]').parent().should('be.visible');
+        cy.get('[data-qa="chart.power"]').should('be.visible');
+
+        //checks all the graph items are visible in the dropdown
+        cy.get('[data-qa="autocomplete.items"]').parent().click();
+        cy.get('div[class*="v-autocomplete__content"]').find('div[class="v-list-item__title"]').each(($el, index) => {
+
+            const title = $el.text().trim();
+            console.log(title);
+            console.log(powerGraphItems[index]);
+            if (title !== powerGraphItems[index]){
+                throw new Error(`Error: ${powerGraphItems[index]} is not found`);
+            }
+        });
+
+
+    })
+});
