6b5149c68b088c9f8e192ed2c191ef494e4113e3
CavanBeardmore <cavan.beardmore@hotmail.co.uk>
2024-01-09 09:59:07 +0000

    Changed logic for installer incentive test:
    now just uses contains rather than custom TS logic

--- cypress/listOfValues.ts
+++ cypress/listOfValues.ts
@@ -1,6 +1,3 @@
-export const rewardsValues: string[] = ["Single-Phase Hybrid", "Single-Phase AC Coupled", "Three-Phase Hybrid", "Three-Phase AC Coupled", "Gateway",
-    "All-In-One", "2.6kWh", "5.2kWh", "8.2kWh", "9.5kWh", "100 points", "200 points", "300 points", "400 points", "500 points"];
-
 export const voucherValues: string[] = ["£100 Amazon voucher", "£200 Amazon voucher", "£300 Amazon voucher", "£400 Amazon voucher", "£500 Amazon voucher"];
 
 export const powerGraphItems: string[] = [
