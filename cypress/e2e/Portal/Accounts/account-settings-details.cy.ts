e7dbd67f16cc0bf406b3193f587730fd6a466e8d
CavanBeardmore <cavan.beardmore@hotmail.co.uk>
2024-01-09 09:09:06 +0000

    Added account settings details test:
    now that the hanging issue ahs been resolved by moving the tests onto Chrome, we can re add the account settings detail test

--- cypress/e2e/Portal/Accounts/account-settings-details.cy.ts
+++ cypress/e2e/Portal/Accounts/account-settings-details.cy.ts
@@ -1,13 +1,6 @@
 import { adminLogin } from "../../../logins";
 import { dashboardSelect, changeDetails } from "../../../funcs";
 
-//this should prevent any tests from hanging
-const time = 180000;
-beforeEach(() => {
-    setTimeout(() => {
-        throw new Error(`Test failed: exceeded run time limit of ${time}ms`);
-    }, time);
-});
 describe("Account settings details", () => {
   it("tests changing the account settings details works", () => {
 
